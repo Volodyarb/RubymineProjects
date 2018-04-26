ba54db7229084bf92111b3c1ff9ed29bfa5e7c6c
Volodyarb <996504@i.ua>
2018-04-26 22:04:19 +0300

    UPDATE1_3

--- untitled4/test.rb
+++ untitled4/test.rb
@@ -3,15 +3,20 @@ require 'pg'
 $con = PG.connect :dbname => 'test',:user => 'volodya'
 
 class People
+  attr_accessor :id, :name, :age
+  def var (id,name, age)
+    @id = id
+    @name = name
+    @age = age
+  end
 
-
-  def select
+  def self.select
     rs= $con.exec("select * from persons ")
     rs.each do |row|
       puts "%-3d |%-10s | %i" % [ row['id'], row['name'], row['age'] ]
     end
   end
-  def insert
+  def self.insert
     print "id: "
     id = gets.chomp
     print "name: "
@@ -22,15 +27,26 @@ class People
     rs.each do |row|
       puts "%s %s %s" % [ row['id'], row['name'], row['age'] ]
     end
-  rescue PG::Error => e
-    puts e.message
-  ensure
-    rs.clear if rs
-    $con.close if $con
   end
+  def self.delete
+    rs = $con.exec "SELECT * FROM persons"
+    print "\nColums: #{rs.fields}"
+    print "\nChoose colums: "
+    colums = gets.chomp
+    print "Choose what to delete: "
+    text = gets .chomp
+    $con.exec("DELETE FROM persons WHERE #{colums} = '#{text}'")
 
-
+  end
 end
 
-#People.new.insert
-#People.new.select
+ choice = People
+    print "Write the method Select,Inesrt,Delete\n"
+    x = gets.chomp.to_s
+if    x == "Select"
+  choice.select
+elsif  x == "Insert"
+  choice.insert
+else x == "Delete"
+  choice.delete
+end
